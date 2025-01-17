8d4083c7745a2c976caa274c9d7fff62a6a7d783
DavidRetmer <mariore@ac.sce.ac.il>
2025-01-17 12:18:32 +0200

    Delete DjangoProject directory

--- DjangoProject/DjangoProject/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from accounts import views
-urlpatterns = [
-  path('',views.home,name='home')
-]
--- DjangoProject/accounts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-# Home page view
-def home(request):
-    return render(request, 'home.html')
