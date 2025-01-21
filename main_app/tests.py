903d1e653963912d95da7ab9cb8b0d6d89741e35
DavidRetmer <mariore@ac.sce.ac.il>
2025-01-21 18:49:28 +0200

    Added tests for the base template and main CSS in main_app/tests.py

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TemplateViewTests(TestCase):
+    def test_base_template(self):
+        response = self.client.get(reverse('home'))  # Asegúrate de que 'home' es el nombre de la URL para la vista que usa base.html
+        self.assertTemplateUsed(response, 'base.html')
+        self.assertContains(response, '<link rel="stylesheet" href="{% static \'styles/main.css\' %}">')
+
+    def test_main_css(self):
+        response = self.client.get(reverse('home'))  # Asegúrate de que 'home' es el nombre de la URL para la vista que usa base.html
+        self.assertContains(response, '/* Barra de navegación */')
+        self.assertContains(response, '.navbar {')
